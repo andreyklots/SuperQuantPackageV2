20002c7a0d3ebf344e41be4cc4bda8af2f83d6a6
andreyklots <56598573+andreyklots@users.noreply.github.com>
2022-01-14 17:26:17 -0600

    Update AlgebraicLinOp.py

--- AlgebraicLinOp.py
+++ AlgebraicLinOp.py
@@ -324,21 +324,22 @@ def kron(A,B):
 
 
 def eigh(A, u_initial):
+    # Find dimensionality of the Linear Operator.
+    VecDim = 1
+    for Factor in A.MatArray[0]:
+        VecDim *= np.shape(Factor)[0]
+    # turn LinearOperator into Scipy Linear Operator
+    spLA_A = spLA.LinearOperator( (VecDim,VecDim), matvec=A.act  )
+
     if np.shape(u_initial) == (): 
     # if u_initial is a scalar, then use scipy.sparse.linalg.eigsh
-        # Find dimensionality of the Linear Operator.
-        VecDim = 1
-        for Factor in A.MatArray[0]:
-            VecDim *= np.shape(Factor)[0]
-        # turn LinearOperator into Scipy Linear Operator
-        spLA_A = spLA.LinearOperator( (VecDim,VecDim), matvec=A.act  )
         u0 = np.array(
                        np.random.rand(VecDim, u_initial)
                      )
-        E, u = spLA.eigsh( A , k=u_initial, which='SA')
+        E, u = spLA.eigsh( spLA_A , k=u_initial, which='SA')
     else:
     # if u_initial is an array/matrix then it is our 0th approx.
     # and use LOBPCG method
         u0 = np.array( u_initial )
-        E, u = spLA.lobpcg( A, u0, largest=False )
+        E, u = spLA.lobpcg( spLA_A, u0, largest=False )
     return E, u
